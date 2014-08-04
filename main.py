64404bf6b0b8e03cd786a45f4173684c0217158a
Garett Dunn <garettmd@gmail.com>
2014-08-04 16:51:23 -0500

    Rework
    Reworking the class and the variables

--- main.py
+++ main.py
@@ -3,45 +3,40 @@ from random import randint
 
 
 class Game:
-    wins = 0
-    losses = 0
-    ties = 0
-    cheats = 0
-    games = 0
-    played = 0
-    finished = ""
 
     def __init__(self):
-        pass
+        self.wins = 0
+        self.losses = 0
+        self.ties = 0
+        self.cheats = 0
+        self.games = 0
+        self.played = 0
+
 
     def whowins(self, compare1, compare2):
-        Game.games += 1
+        self.played -= 1
         if compare1 == compare2:
-            Game.ties += 1
-            Game.played += 1
+            self.ties += 1
             print("You tied")
         elif (compare1 - compare2) % 3 == 1:
-            Game.wins += 1
-            Game.played += 1
+            self.wins += 1
             print("You won!")
         else:
-            Game.losses += 1
-            Game.played += 1
+            self.losses += 1
             print("You lost...")
-        if Game.games == Game.played:
-            Game.finished = "yes"
 
 
 def main():
+    game = Game()
     try:
-        Game.games = int(input("How many games of RPS would you like to play?"))
+        game.games = int(input("How many games of RPS would you like to play?"))
     except ValueError:
         print("Please enter a number")
     else:
-        while Game.played <= Game.games:
-            computer = randint(0, 2)
-            choice = input("Choose your weapon - (R)ock, (P)aper, or (S)cissors").lower()
-            game = Game()
+        computer = randint(0, 2)
+        choice = input("Choose your weapon - (R)ock, (P)aper, or (S)cissors").lower()
+
+        while game.played > 0:
             if choice == "r":
                 player = 0
                 game.whowins(player, computer)
@@ -53,9 +48,9 @@ def main():
                 game.whowins(player, computer)
             else:
                 print("You have only have three choices, stop trying to make up other weapons.")
-                Game.cheats += 1
-        print("You won {0} games, lost {1}, and tied for {2}.".format(wins,losses,ties))
-        if Game.cheats > 0:
+                game.cheats += 1
+        print("You won {0} games, lost {1}, and tied for {2}.".format(game.wins,game.losses,game.ties))
+        if game.cheats > 0:
             print("You also cheated {0} times, you cheater.".format(cheats))
 
 main()
